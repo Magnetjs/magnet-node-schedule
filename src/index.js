45386f4e5840a6e8add547c5deba0d8b09cfcf1d
Kieve Chua <kievechua@gmail.com>
2017-02-25 13:59:27 +0800

    Convert to typescript and upgrade core

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Base from 'magnet-core/base'
-import schedule from 'node-schedule'
-
-export default class NodeSchedule extends Base {
-  async setup () {
-    this.app.nodeSchedule = schedule
-  }
-}
